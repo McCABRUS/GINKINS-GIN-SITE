1df139e8cbc75750d2ff7554c56213efd15a6728
Christian Bodensiek <christianbodensiek@gmail.com>
2026-01-30 11:59:07 -0500

    Added Welcome Section

---
+++ app/components/WelcomeHome.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+export default function WelcomeHome() {
+  return (
+    <section className="bg-(--primary-red-main) flex w-full h-51.25 flex-col justify-center items-center gap-2.5">
+      <div className="flex flex-wrap flex-row justify-between items-center content-center w-80.5 md:w-160.75 h-16.5 bg-[url(/WelcomeContainer/welcomeContainer.svg)] bg-contain bg-center bg-no-repeat">
+        <div>
+          <Image
+            src="/WelcomeContainer/iconLeft.svg"
+            alt="Left Icon"
+            height={42}
+            width={20}
+            className="ml-3.5 hidden md:block"
+          />
+          <Image
+            src="/WelcomeContainer/iconLeft_m.svg"
+            alt="Left Icon"
+            height={21}
+            width={10}
+            className="ml-3.5 block md:hidden"
+          />
+        </div>
+        <div className="m-0">
+          <h2 className="text-background! text-lg! md:text-4xl!">
+            WELCOME TO GINKINS
+          </h2>
+        </div>
+        <div>
+          <Image
+            src="/WelcomeContainer/iconRight.svg"
+            alt="Left Icon"
+            height={42}
+            width={20}
+            className="mr-3.5 hidden md:block"
+          />
+          <Image
+            src="/WelcomeContainer/iconRight_m.svg"
+            alt="Left Icon"
+            height={21}
+            width={10}
+            className="mr-3.5 block md:hidden"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
