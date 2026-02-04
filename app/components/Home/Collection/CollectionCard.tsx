14c57f4634fd7605fc09df05458c11bcbdf288d0
Christian Bodensiek <christianbodensiek@gmail.com>
2026-02-03 19:05:27 -0500

    Set responsive to Collection home section

--- app/components/Home/Collection/CollectionCard.tsx
+++ app/components/Home/Collection/CollectionCard.tsx
@@ -9,8 +9,8 @@ interface CardProps {
 
 export default function CollectionCard({ title, text, img, alt }: CardProps) {
   return (
-    <div className="h-full w-181 flex shrink-0 cursor-pointer">
-      <div className="shrink-0 w-95 h-[672.99px] relative">
+    <div className="h-full w-181 lg:flex shrink-0 cursor-pointer">
+      <div className="shrink-0 w-full lg:w-95 h-86 lg:h-168.25 relative mt-8 lg:mt-0">
         <Image
           className="object-contain filter-[drop-shadow(50px_-15px_40px_#000)]"
           fill
@@ -22,16 +22,16 @@ export default function CollectionCard({ title, text, img, alt }: CardProps) {
           style={{ filter: 'blur(5.72px)' }}
         ></div>
       </div>
-      <div className="pb-16.5 flex flex-col gap-13.5 items-start justify-start shrink-0 w-82 relative top-30">
-        <div className="flex flex-col gap-8.25 items-start justify-start self-stretch shrink-0 relative flex-1">
-          <h2 className="text-(--primary-gold-main)! text-left w-65 whitespace-pre-wrap uppercase relative self-stretch">
+      <div className="pb-16.5 flex flex-col gap-13.5 items-start justify-start shrink-0 w-full lg:w-82 relative top-5 lg:top-30">
+        <div className="flex flex-col gap-8.25 items-start justify-start self-stretch shrink-0 relative flex-1 px-25 lg:px-0">
+          <h2 className="text-(--primary-gold-main)! text-center lg:text-left w-full lg:w-65 whitespace-pre-wrap uppercase relative self-stretch">
             {title}
           </h2>
           <div className="text-background! text-left leading-[24.16px] self-stretch h-37 whitespace-pre-wrap grow">
             {text}
           </div>
         </div>
-        <div className="flex flex-col gap-2.5 items-start justify-start shrink-0 relative -top-25">
+        <div className="flex flex-col gap-2.5 items-start justify-start shrink-0 relative -top-7 lg:-top-25 mx-auto lg:mx-0">
           <button className="inline-flex items-center justify-center bg-(--primary-red-main) px-8 py-3 text-sm font-medium uppercase tracking-wide text-white transition hover:bg-(--primary-gold-main)">
             FIND GINKINS NEAR TO YOU
           </button>
--- app/components/Home/Collection/CollectionHome.tsx
+++ app/components/Home/Collection/CollectionHome.tsx
@@ -9,32 +9,33 @@ export default function CollectionHome() {
           transform: 'rotate(90deg) scale(1, 1)',
         }}
       ></div>
-      <div className="bg-(--primary-black) w-full h-[1270.64px] absolute left-0"></div>
+      <div className="bg-(--primary-black) w-full h-380 md:h-337.5 lg:h-317.5 absolute left-0"></div>
       <div className="flex flex-col gap-6 items-center justify-start w-full absolute top-23">
         <div className="p-2.5 flex flex-row gap-2.5 items-center justify-center shrink-0 w-113.5 relative">
           <h3 className="text-(--primary-gold-main)! text-center uppercase relative flex-1">
-            The Gin Collection (Our Craft, Your Experience)
+            The Gin Collection (Our Craft, <br className="inline lg:hidden" />
+            Your Experience)
           </h3>
         </div>
         <div className="p-2.5 flex flex-col gap-2.5 items-center justify-center self-stretch shrink-0 relative">
           <h1 className="text-background! text-center font-global-tokens-headings-h1-desktop-font-family text-global-tokens-headings-h1-desktop-font-size leading-global-tokens-headings-h1-desktop-line-height font-global-tokens-headings-h1-desktop-font-weight uppercase relative self-stretch">
-            THREE EXPRESSIONS.
+            THREE <br className="inline xl:hidden" /> EXPRESSIONS.
             <br />
-            ONE SPIRIT OF CONNECTION{' '}
+            ONE SPIRIT OF <br className="inline xl:hidden" /> CONNECTION{' '}
           </h1>
         </div>
       </div>
       <CollectionCarousel></CollectionCarousel>
-      <div className="w-full h-317.25 static -mt-204.5">
+      <div className="w-full h-300 md:h-257 lg:h-317.25 static -mt-204.5">
         <div
-          className="w-108 h-317.25 absolute left-0 top-0"
+          className="w-108 h-317.25 absolute left-0 top-0 hidden lg:block"
           style={{
             background:
               'linear-gradient(270deg, rgba(30, 30, 30, 0.00) 0%,rgba(30, 30, 30, 1.00) 92.30769276618958%)',
           }}
         ></div>
         <div
-          className="w-123.5 h-317.25 absolute right-0 top-317"
+          className="w-123.5 h-317.25 absolute right-0 top-317  hidden lg:block"
           style={{
             background:
               'linear-gradient(270deg, rgba(30, 30, 30, 0.00) 33.01192224025726%,rgba(30, 30, 30, 1.00) 100%)',
@@ -43,7 +44,7 @@ export default function CollectionHome() {
           }}
         ></div>
       </div>
-      <div className="flex flex-col gap-11 w-full absolute px-37.25  top-266.5">
+      <div className="flex flex-col gap-11 w-full absolute px-8 md:px-37.25  top-266.5">
         <div
           className="border-solid border-[#e3d384] border-t -mt-14 border-r-0 border-b-0 border-l-0 self-stretch shrink-0 h-0 relative"
           style={{
@@ -51,21 +52,21 @@ export default function CollectionHome() {
             transform: 'rotate(0deg) scale(1, 1)',
           }}
         ></div>
-        <div className="flex flex-row gap-[97.64px] items-start justify-center self-stretch shrink-0 h-[216.43px] relative">
-          <div className="flex flex-row gap-[51.34px] items-start justify-center flex-1 h-[216.43px] relative">
-            <div className="text-background text-left text-[16.10613441467285px] leading-[24.16px] relative flex-1 h-[147.98px]">
+        <div className="flex flex-row items-start justify-center self-stretch shrink-0 h-[216.43px] relative mt-20 md:mt-0">
+          <div className="flex flex-col lg:flex-row gap-0 items-start justify-center flex-1 h-[216.43px] relative">
+            <div className="text-background text-left text-base leading-[24.16px] relative flex-1 h-[147.98px] mb-20 md:mb-0">
               Every bottle of Ginkins is a celebration—of tradition reimagined,
               of botanicals brought to life, and of the shared joy in
               discovering something truly exceptional.{' '}
             </div>
             <div
-              className="border-solid border-background border-t mt-27 border-r-0 border-b-0 border-l-0 shrink-0 w-[216.43px] h-0 relative"
+              className="border-solid border-background border-t mt-122.5 md:mt-77.5 lg:mt-27 border-r-0 border-b-0 border-l-0 shrink-0 w-16.25 lg:w-54 h-0 absolute lg:relative left-1/2 lg:left-0 -translate-x-1/2 lg:translate-x-0"
               style={{
                 transformOrigin: 'top',
                 transform: 'rotate(90deg) scale(1, 1)',
               }}
             ></div>
-            <div className="text-background text-left text-[16.10613441467285px] leading-[24.16px] font-normal relative flex-1">
+            <div className="text-background text-left text-base leading-[24.16px] font-normal relative flex-1">
               Our collection reflects a devotion to detail, a passion for
               people, and a belief that the best gin doesn’t just taste great—it
               makes you feel something. Whether you’re savoring a quiet moment
