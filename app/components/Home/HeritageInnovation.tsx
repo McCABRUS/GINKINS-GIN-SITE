c5c98b7e611d6ef415d5d2f88e0978c2c360a00d
Christian Bodensiek <christianbodensiek@gmail.com>
2026-02-03 23:10:24 -0500

    Set responsive to Heritage Innovation home section

--- app/components/Home/HeritageInnovation.tsx
+++ app/components/Home/HeritageInnovation.tsx
@@ -1,27 +1,36 @@
 import Image from 'next/image';
 export default function HeritageInnovation() {
   return (
-    <section className="w-screen bg-(--primary-red-main) py-37">
-      <div className="mx-auto max-w-350 px-6">
-        <div className="flex items-center justify-between gap-12">
+    <section className="w-screen bg-(--primary-red-main) h-182.5 md:h-auto py-20 md:py-37">
+      <div className="mx-auto max-x-full md:max-w-350 px-6">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-12">
           <div>
-            <h3 className="block uppercase tracking-widest text-(--primary-gold-main)! mb-6">
+            <div className="mx-auto h-15 w-15 flex md:hidden mb-16.5">
+              <Image
+                src="/ginkins-isotype.svg"
+                alt="ginkins isotype"
+                height={60}
+                width={60}
+              />
+            </div>
+            <h3 className="block text-center md:text-left tracking-widest text-(--primary-gold-main)! mb-4.25 md:mb-6">
               The Ginkins Story
             </h3>
 
-            <h1 className="leading-[1.05] text-background! uppercase">
+            <h1 className="text-center md:text-left text-background! mb-16.5 md:mb-0">
               A Heritage
               <br />
-              of Innovation
+              of <br className="inline md:hidden" />
+              Innovation
             </h1>
           </div>
           <div className="flex gap-10 flex-col items-end justify-end">
-            <p className="text-[36px] font-cormorant-garamond leading-snug text-(--primary-gold-main) text-right italic max-w-xs">
+            <p className="text-4xl font-cormorant-garamond leading-snug text-(--primary-gold-main) text-right italic max-w-full md:max-w-xs">
               Rooted in Louisville.
               <br />
               Crafted to Inspire.
             </p>
-            <div className="flex h-24 w-24">
+            <div className="h-24 w-24 hidden md:flex">
               <Image
                 src="/ginkins-isotype.svg"
                 alt="ginkins isotype"
