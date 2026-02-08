e27095591c4c98ffdc46ad53d04ea2e1591a2ac1
Christian Bodensiek <christianbodensiek@gmail.com>
2026-02-07 20:57:01 -0500

    Added Celebrated banner - Accolades section

--- app/accolades/page.tsx
+++ app/accolades/page.tsx
@@ -1,4 +1,5 @@
 import AccoladesHero from '../components/Accolades/AccoladesHero';
+import Celebrated from '../components/Accolades/Celebrated';
 import ShopBannerCocktails from '../components/Cocktails/ShopBannerCocktails';
 import FooterMerch from '../components/FooterMerchCarousel/FooterMerch';
 
@@ -7,6 +8,7 @@ export default function Subscribe() {
     <div className="flex min-h-screen items-center justify-center overflow-x-hidden">
       <main className="flex min-h-screen w-full flex-col items-center justify-between">
         <AccoladesHero />
+        <Celebrated />
         <ShopBannerCocktails />
         <FooterMerch />
       </main>
---
+++ app/components/Accolades/Celebrated.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+
+export default function Celebrated() {
+  return (
+    <section className="relative aspect-3/5 md:aspect-16/7 bg-cover bg-center  w-full z-10">
+      <div
+        className="absolute inset-0 bg-cover bg-center "
+        style={{
+          backgroundImage:
+            "url('/accolades/ginkins-gin-serving-martini-box.png')",
+        }}
+      />
+      <div className="pointer-events-none absolute inset-0 vignette-overlay" />
+      <div className="relative mx-auto px-43 py-16.5 h-full">
+        <div className="grid grid-cols-1 gap-16 lg:grid-cols-2 lg:gap-20 h-full">
+          <div className="flex items-baseline">
+            <h4 className="text-background!">
+              Celebrated by Experts. <br />
+              Loved by Explorers.
+            </h4>
+          </div>
+          <div className="relative">
+            <span
+              className="absolute -left-12 top-0 hidden h-[85%] w-px bg-background lg:block"
+              aria-hidden
+            />
+            <span
+              className="mb-12 block h-px w-full bg-background lg:hidden"
+              aria-hidden
+            />
+            <div className="space-y-6">
+              <p className="text-base leading-6 font-normal text-background">
+                We don’t make gin for trophies—but it’s nice when the world
+                notices. From prestigious competitions to top-tier press
+                coverage, Ginkins has earned recognition for doing things our
+                way:
+              </p>
+              <p className="text-4xl italic font-cormorant-garamond text-(--primary-gold-main)">
+                Bold, refined, and rooted in connection.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Image
+        src="/accolades/ginkins-gin-icon-sparkle-red.svg"
+        alt="Sparkle icon"
+        height={110}
+        width={113}
+        className="absolute -bottom-13.75 left-1/2 -translate-x-1/2"
+        aria-hidden
+      />
+    </section>
+  );
+}
