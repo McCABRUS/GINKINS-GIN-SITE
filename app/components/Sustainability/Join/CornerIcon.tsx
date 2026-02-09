99f5d159e30c83046c3b5e6ddfab8f6ac2a4f949
Christian Bodensiek <christianbodensiek@gmail.com>
2026-02-08 20:50:04 -0500

    Minor adjustment

--- app/components/Sustainability/Join/CornerIcon.tsx
+++ app/components/Sustainability/Join/CornerIcon.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function CornerIcon({ className }: Props) {
   return (
     <Image
-      src="/Sustainability/FlowerIcon.svg"
+      src="/sustainability/FlowerIcon.svg"
       alt="Red stylized botanical flower icon for Ginkins Gin visual identity"
       width={83}
       height={83}
