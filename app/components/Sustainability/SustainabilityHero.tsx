bad6505bf079a4f28d356d57ceb3d92a04729090
Christian Bodensiek <christianbodensiek@gmail.com>
2026-02-08 19:54:40 -0500

    Set responsive to Sustainability Hero section

--- app/components/Sustainability/SustainabilityHero.tsx
+++ app/components/Sustainability/SustainabilityHero.tsx
@@ -15,8 +15,8 @@ export default function SustainabilityHero() {
       <div className="relative  text-center">
         <h2 className="mb-6 text-center">Sustainability</h2>
         <h1 className="text-center">The Ginkins Way</h1>
-        <div className="my-16 flex justify-center">
-          <div className="h-48 w-48">
+        <div className="my-13.75 lg:my-16 flex justify-center">
+          <div className="w-32.75 h-32.75 lg:h-48 lg:w-48">
             <Image
               src="/sustainability/ginkins-gin-icon-botanical-flower-red.svg"
               alt="Leaves icon"
@@ -31,7 +31,7 @@ export default function SustainabilityHero() {
         </div>
       </div>
       <div
-        className="absolute border-solid border-(--primary-red-main) border-t border-r-0 border-b-0 border-l-0 shrink-0 w-18.75 h-0 z-20 bottom-51.25 left-1/2 -translate-x-1/2"
+        className="absolute border-solid border-(--primary-red-main) border-t border-r-0 border-b-0 border-l-0 shrink-0 w-18.75 h-0 z-20 bottom-48 lg:bottom-51.25 left-1/2 -translate-x-1/2"
         style={{
           transform: 'rotate(90deg) scale(1, 1)',
         }}
