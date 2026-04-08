8075845315917571c4dce382aebc99073bd51fa8
Christian Bodensiek <christianbodensiek@gmail.com>
2026-04-07 22:33:39 -0500

    Adjusted Robots and Sitemap to static export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from 'next';
-
+export const dynamic = 'force-static';
 const baseUrl = 'https://ginkinsgin.com';
 
 export default function sitemap(): MetadataRoute.Sitemap {
